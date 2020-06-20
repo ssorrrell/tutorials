188aaf4d7cc022eaeb9d7929f2536afa8476806c
ssorrrell <ssorrrell@hotmail.com>
2020-06-20 14:15:14 -0500

    Add Xamarin Tutorial projects

---
+++ WebServiceTutorial/WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServiceTutorial
+{
+    class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "7a3a1cdf228700bf30c951a0a7162b8a";
+    }
+}
